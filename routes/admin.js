583abd3026e0841f6f394f1543674f46486e45c2
Kra7ms <kandira.123@gmail.com>
2023-10-19 07:11:01 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-router.use("/admin/create", function(req, res, next) {
+router.use("/create", function(req, res, next) {
     res.sendFile(path.join(__dirname, "../views/admin", "admin-create.html"))
 })
-router.use("/admin/detay", function(req, res, next) {
+router.use("/detay", function(req, res, next) {
     res.sendFile(path.join(__dirname, "../views/admin", "admin-detay.html"))
 })
-router.use("/admin/x", function(req, res, next) {
+router.use("/x", function(req, res, next) {
     res.sendFile(path.join(__dirname, "../views/admin", "admin-x.html"))
 })
 module.exports = router
