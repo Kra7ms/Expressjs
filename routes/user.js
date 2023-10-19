6cbc66b53850cb2ec466e6e9df6c8f2c7d4503ad
Kra7ms <kandira.123@gmail.com>
2023-10-19 06:46:09 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const path = require('path')
 
 router.use("/x/:id", function(req, res, next) {
-    res.sendFile(path.join(__dirname, "../views/files", "detay.html"))
+    res.sendFile(path.join(__dirname, "../views/user", "detay.html"))
 })
 router.use("/x", function(req, res, next) {
-    res.sendFile(path.join(__dirname, "../views/files", "x.html"))
+    res.sendFile(path.join(__dirname, "../views/user", "x.html"))
 })
 router.use("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/files", "index.html"))
+    res.sendFile(path.join(__dirname, "../views/user", "index.html"))
 })
 module.exports = router
