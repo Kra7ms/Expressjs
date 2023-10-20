f75c7fe5ca8139576d6403792ac668a80b4501d6
Kra7ms <kandira.123@gmail.com>
2023-10-20 06:38:20 +0300

    Template Engines
    ejs

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const router = express.Router()
 
+const data = {
+    title: "Anasayfa",
+    catagories: ["Ahmet", "Mahmut", "Neriman", "Can", "Tarık"],
+    blogs: [
+        {
+            blogid: 1,
+            blogtitle: "Raspberry Pi",
+            blogexplanation: "Raspberry Pi explanation",
+            image: "1.jpg",
+            anasayfa: true
+        },
+        {
+            blogid: 2,
+            blogtitle: "Arduino",
+            blogexplanation: "Arduino explanation",
+            image: "2.jpg",
+            anasayfa: true
+        },
+        {
+            blogid: 3,
+            blogtitle: "Solidworks",
+            blogexplanation: "Solidworks explanation",
+            image: "3.avif",
+            anasayfa: false
+        }
+    ]
+}
+
 router.use("/x/:id", function(req, res, next) {
     res.render("user/detay")
 })
 router.use("/x", function(req, res, next) {
-    res.render("user/x")
+    res.render("user/x", data)
 })
 router.use("/", function(req, res) {
-    res.render("user/index")
+    res.render("user/index", data)
 })
 module.exports = router
