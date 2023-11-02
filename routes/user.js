a60ffdc7801f481a71e2bcaf91f539ea8b906057
Kra7ms <kandira.123@gmail.com>
2023-11-02 18:40:56 +0300

    Hatalı var

--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../data/db')
-const data = {
-    title: "Anasayfa",
-    catagories: ["Ahmet", "Mahmut", "Neriman", "Can", "Tarık"],
-    blogs: [
-        {
-            blogid: 1,
-            blogtitle: "Raspberry Pi",
-            blogexplanation: "Raspberry Pi explanation",
-            image: "1.jpg",
-            anasayfa: true,
-        },
-        {
-            blogid: 2,
-            blogtitle: "Arduino",
-            blogexplanation: "Arduino explanation",
-            image: "2.jpg",
-            anasayfa: true
-        },
-        {
-            blogid: 3,
-            blogtitle: "Solidworks",
-            blogexplanation: "Solidworks explanation",
-            image: "3.avif",
-            anasayfa: false
-        }
-    ]
-}
+
 
 router.use("/x/:id", function(req, res) {
     res.render("user/detay")
