1b181a3d164cbcee022b52533f8341e720623d5a
Kra7ms <kandira.123@gmail.com>
2023-11-02 14:38:35 +0300

    sql

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+    host: "localhost",
+    user: "root",
+    password: "Tesekkurler4080",
+    database: "blogapp"
+    }
+}
+
+module.exports = config
--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const app = express()
 const userRoutes = require('./routes/user')
 const adminRoutes = require('./routes/admin')
 app.set('view engine', 'ejs')
+const mysql = require('mysql2')
+const config = require('./config')
 
+let connection = mysql.createConnection(config.db)
+connection.connect(function(err){
+    if(err){
+        return console.log(err)
+    }
+    connection.query("select * from blog", function(err, result){
+        console.log(result[0].blogtitle)
+    })
+    console.log("The MySQL server is connected and ready to use")
+})
 app.get("view engine")
 app.use("/libs", express.static(path.join(__dirname, "node_modules")))
 app.use("/static", express.static(path.join(__dirname, "public")))
