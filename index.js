b09c3bb6c52e26ab7c6aaf2f33822370d4ee5f6d
Kra7ms <kandira.123@gmail.com>
2023-11-08 21:27:19 +0300

    run

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const path = require('path')
 const app = express()
+app.set('view engine', 'ejs')
+const path = require('path')
 const userRoutes = require('./routes/user')
 const adminRoutes = require('./routes/admin')
-app.set('view engine', 'ejs')
 
-app.get("view engine")
 app.use("/libs", express.static(path.join(__dirname, "node_modules")))
 app.use("/static", express.static(path.join(__dirname, "public")))
+app.get("view engine")
 
 app.use("/admin", adminRoutes)
 app.use(userRoutes)
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,69 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../data/db')
+const db = require('../data/db.js')
 
 
+const data = {
+    title: "Popüler Konular",
+    catagories: ["Science", "Tecnology", "Engineering", "Art", "Mathmatics"],
+    blogs: [
+        {
+            blogid: 1,
+            title: "Raspberry Pi",
+            explanation: "The Raspberry Pi is a low cost, credit-card sized computer that plugs into a computer monitor or TV, and uses a standard keyboard and mouse",
+            image: "1.jpg",
+            homepage: true
+        },
+        {
+            blogid: 2,
+            title: "Arduino",
+            explanation: "Open-source electronic prototyping platform enabling users to create interactive electronic objects.",
+            image: "2.jpg",
+            homepage: true
+        },
+        {
+            blogid: 3,
+            title: "SolidWorks",
+            explanation: "SolidWorks is a 3D parametric design software that is used to design all sorts of products such as automobiles, marine equipment, airplane parts, cell phones, cameras, furniture, electrical assemblies, glasses, lighting fixtures, toys, vacuum cleaners, or any other like product you can think of.",
+            image: "3.avif",
+            homepage: true
+        },
+        {
+            blogid: 4,
+            title: "Excel",
+            explanation: "Excel is a spreadsheet program from Microsoft and a component of its Office product group for business applications.",
+            image: "4.jpg",
+            homepage: false
+        }
+    ]
+}
 router.use("/x/:id", function(req, res) {
-    res.render("user/detay")
-})
-router.use("/x", function(req, res) {
     db.execute("select * from blog")
         .then(result => {
             db.execute("select * from catagory")
                 .then(result2 => {
                     res.render("user/x", {
+                        title: "Naber",
+                        blogs: result,
+                        categories: result2
+
+                })
+                .catch(err => console.log(err))
+            })
+        .catch(err => console.log(err))
+        })
+})
+
+router.use("/x", function(req, res) {
+    res.render("user/x")
+})
+
+router.use("/", function(req, res) {
+    db.execute("select * from blog")
+        .then(result => {
+            db.execute("select * from catagory")
+                .then(result2 => {
+                    res.render("user", {
                         title: "Naber",
                         blogs: result[0],
                         catagories: result2[0]
@@ -21,20 +73,5 @@ router.use("/x", function(req, res) {
             })
         .catch(err => console.log(err))
         })
-})
-router.use("/", function(req, res) {
-    db.execute("select * from blog where anasayfa=1")
-        .then(result => {
-            db.execute("select * from catagory")
-                .then(result => {
-                    res.render("user/index", {
-                        title: "Anasayfa",
-                        blogs: result[0],
-                        catagories: result[0]
-                    })
-                })
-        .catch(err => console.log(err))
-        })
-        .catch(err => console.log(err))
 })
 module.exports = router
